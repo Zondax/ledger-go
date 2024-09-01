92519577fd019e6160c3b15f4016de016cc04135
Juan Leni <juan.leni@zondax.ch>
2024-09-01 12:03:06 +0200

    upgrading deps (#34)
    * upgrading deps
    
    * upgrade go version

--- ledger_hid.go
+++ ledger_hid.go
@@ -2,7 +2,7 @@
 // +build !ledger_mock,!ledger_zemu
 
 /*******************************************************************************
-*   (c) 2018 - 2022 ZondaX AG
+*   (c) Zondax AG
 *
 *  Licensed under the Apache License, Version 2.0 (the "License");
 *  you may not use this file except in compliance with the License.
